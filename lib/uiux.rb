65aeada531ee941ccc3f4fc0ac9da353a6818802
Endri Gjiri <egjiri@gmail.com>
2015-11-01 10:27:01 -0500

    Adds the gem's functionality

--- lib/uiux.rb
+++ lib/uiux.rb
@@ -1,5 +1,51 @@
 require 'uiux/version'
+require 'rainbow/ext/string'
 
-module Uiux
-  # Your code goes here...
+# Renamed UIUX module to just UI for simplicity of calling it
+module UI
+  def self.start(message, &block)
+    start_heading message
+    yield block
+    finished
+  rescue Exception => error
+    puts "\nFailed  #{'↓' * 61} #{time}".color(:red)
+    raise error
+  end
+
+  def self.heading(message)
+    puts "\n#{message}".color(:green)
+  end
+
+  def self.message(message)
+    puts "#{message}".color(:white)
+  end
+
+  def self.error(message) # Rename to fail one in the gem
+    fail "ERROR: #{message}".color(:red)
+  end
+
+  def self.execute(command)
+    puts '==> '.color(:blue) + command.color('#7F461D') # Brown
+    puts result = `#{command}`
+    result
+  end
+
+  def self.time
+    time = Time.respond_to?(:current) ? Time.current : Time.now.utc
+    "🕑  #{time} 🕑 ".color(:white)
+  end
+
+  private
+
+  def self.start_heading(message)
+    fail if message.length > 70
+    puts '='.color(:cyan) * 100
+    print message.color(:cyan)
+    puts ' ' * (70 - message.length) + time
+  end
+
+  def self.finished
+    puts "\nFinished#{' ' * 61} #{time}".color(:magenta)
+    puts '='.color(:magenta) * 100
+  end
 end
--- lib/uiux/version.rb
+++ lib/uiux/version.rb
@@ -1,3 +1,3 @@
-module Uiux
+module UIUX
   VERSION = '0.1.0'
 end
